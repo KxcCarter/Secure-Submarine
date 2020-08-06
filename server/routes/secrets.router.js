55e5fa3bf0b5dd0352c5cb2d85a2264a2e5e85bd
Kenneth Carter <kxccarter@gmail.com>
2020-08-06 14:15:27 -0500

    Only users with correct clearance level can view secrets.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+router.get('/', rejectUnauthenticated, (req, res) => {
+  console.log('req.user:', req.user);
+  const clearanceLevel = req.user.clearance_level;
+  const query = `SELECT "secret".* FROM "secret"
+                WHERE "secret".secrecy_level < $1;`;
+
+  pool
+    .query(query, [clearanceLevel])
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log('Error making SELECT for secrets:', error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
